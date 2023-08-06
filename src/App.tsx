34fe1c29d4d46d223ae10df44885dfeebb2ea327
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2023-08-05 22:06:10 -0400

    style: :art: Tailwindcss basic configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+/**
+ * npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch
+ */
+function App() {
+  return (
+    <>
+      <p className="">KEK</p>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}",".index.html"],
+  theme: {
+    colors: {
+      "primary": "#150C18",
+      "secondary": "#1E1122",
+      "contrast": "#8A479E",
+      "shadow-bg": "rgba(0, 0, 0, 0.8)",
+      "selected-carrousel": "#FCFF6B",
+    },
+  },
+  plugins: [],
+}
+
